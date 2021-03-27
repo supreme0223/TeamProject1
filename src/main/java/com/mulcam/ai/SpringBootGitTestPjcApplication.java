cfcc631d3b65acab3954a689b8600ee934cd423a
Jake Park <supremeletra@gmail.com>
2021-03-27 13:51:58 +0900

    [Jake Park] Initial Project Organization

---
+++ src/main/java/com/mulcam/ai/SpringBootGitTestPjcApplication.java
@@ -0,0 +1,13 @@
+package com.mulcam.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestPjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitTestPjcApplication.class, args);
+	}
+
+}
